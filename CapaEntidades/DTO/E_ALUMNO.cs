c068f0d31d0c11a08cc45ab82774a004aa8b61fd
Fernando Jose Serrato Vilcherres <svilcherresfern@crece.uss.edu.pe>
2020-07-15 20:39:39 -0500

    Cambios

--- CapaEntidades/DTO/E_ALUMNO.cs
+++ CapaEntidades/DTO/E_ALUMNO.cs
@@ -9,7 +9,7 @@ namespace CapaEntidades.DTO
     public class E_ALUMNO
     {
         public int AL_IDALUMNO { get; set; }
-        public int PE_IDPERSONA { get; set; }
+        public int PE_IDPERSONA { get; set; } 
         public int PER_IDAPODERADO { get; set; }
 
         public E_ALUMNO(int pER_IDAPODERADO)
